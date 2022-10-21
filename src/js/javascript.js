e85f3bf31ea591cc5c85aeb6f66e7f4a7e54f023
afelty <afelty15@gmail.com>
2022-10-20 20:06:48 -0500

    Fix DOM calls so init method fires without errors

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,30 +1,40 @@
+
 //controls game mechanics
 var controlFlow = {
-    
+
     //initialize specific things
     init: function() {
         this.cacheDom();
         this.render();
-        //this.bindevents()?
-        
+        this.playerOne = this.playerCreator("one")
+        this.playerTwo = this.playerCreator("two")
+        this.gameBoard = {
+            myBoard: ["n", "n", "n", "n", "n", "n", "n", "n", "n"],
+            finished: false,
+            whichPlayer: "1"
+            }
+        //this.bindevents()?      
     },
 
     //saves all DOM searches
     cacheDom: function() {
-        this.$el = $('.board');
-        this.$buttons = this.$el.getElementsByClassName("choiceButton");
+        this.$el = document.querySelector('.board');
+        this.$buttons = this.$el.querySelectorAll(".choiceButton");
         
         //add event where when button is clicked, it sets the cell
+        //passes in string containing only cell number
         this.$buttons.forEach(element => {
             element.addEventListener('click', () => setCell(element.id.slice(-1)));
         });
     },
 
     //sets cell depending on player click; chance current player
-    setCell: function(btnId) {
-        addChoice(btnId);
+    //input: string that is cell number
+    setCell: function(cellNum) {
+        addChoice(cellNum);
+        console.log("hello!")
 
-
+        //change current player
         if (this.gameBoard.whichPlayer === '1') {
             this.gameBoard.which = '2'
         } else {this.gameBoard.whichPlayer = '1'}
@@ -34,23 +44,13 @@ var controlFlow = {
 
     //refreashes screen to show correct board
     render: function() {
-
+     
     },
 
     //adds current choice to correct place in game array
-    addChoice: function(btnId) {
-        this.gameBoard.myBoard[btnId] = this.gameBoard.whichPlayer;
-    },
-
-    //need bind events?
-
-
-
-    //gameBoard object storing current state of game
-    gameBoard: {
-    myBoard: ["n", "n", "n", "n", "n", "n", "n", "n", "n"],
-    finished: false,
-    whichPlayer: "1"
+    //input: string that is cell number
+    addChoice: function(cellNum) {
+        this.gameBoard.myBoard[cellNum] = this.gameBoard.whichPlayer;
     },
 
     playerCreator: function(player) {
@@ -58,11 +58,13 @@ var controlFlow = {
         let result =  "Haven't won yet";
     }
 
+     //need bind events?
+
+    
+
 }
 
-let playerOne = playerCreator("one")
-let playerTwo = playerCreator("two")
+controlFlow.init();
 
 
 
-//logic for checking if won;
