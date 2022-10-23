10740deaa8cba338b039604eda454f81e334d842
afelty <afelty15@gmail.com>
2022-10-23 14:39:15 -0500

    Create end game methods

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -11,7 +11,11 @@ var controlFlow = {
         this.gameBoard = {
             myBoard: ["n", "n", "n", "n", "n", "n", "n", "n", "n"],
             finished: false,
-            whichPlayer: "1"
+            whichPlayer: "1",
+            winPlayer: "",
+            playerOneScore: 0,
+            playerTwoScore: 0,
+            numPlays: 0
             }
         //this.bindevents()?      
     },
@@ -21,6 +25,7 @@ var controlFlow = {
         this.$el = document.querySelector('.board');
         this.$buttons = this.$el.querySelectorAll(".choiceButton");
         this.$images = this.$el.querySelectorAll('img');
+        this.$displayMessage = this.#el.querySelector('.center');
         console.log(this.$images);
         //add event where when button is clicked, it sets the cell
         //passes in string containing only cell number
@@ -43,7 +48,9 @@ var controlFlow = {
             this.gameBoard.whichPlayer = '1';
         }
 
-        this.render();
+        if (this.isDone()) {
+            this.endGame();
+        }
     },
 
     //refreashes screen to show correct board
@@ -56,6 +63,7 @@ var controlFlow = {
     addChoice: function(cellNum) {
         this.gameBoard.myBoard[cellNum - 1] = this.gameBoard.whichPlayer;
         console.log(this.gameBoard.myBoard);
+        this.gameBoard.numPlays++;
     },
 
     playerCreator: function(player) {
@@ -70,8 +78,50 @@ var controlFlow = {
         } else {
             this.$images[cellNum - 1].src = "./images/o.png";
         }
-    }
+    },
 
+    isDone: function() {
+        if (this.gameBoard[0] === this.gameBoard[1] && this.gameBoard[1] == this.gameBoard[2]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[0]
+        } else if (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[4] == this.gameBoard[5]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[3]
+        } else if (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[7] == this.gameBoard[8]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[6]
+        } else if (this.gameBoard[0] === this.gameBoard[3] && this.gameBoard[3] == this.gameBoard[6]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[0]
+        } else if (this.gameBoard[1] === this.gameBoard[4] && this.gameBoard[4] == this.gameBoard[7]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[1]
+        } else if(this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[5] == this.gameBoard[8]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[2]
+        } else if(this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[4] == this.gameBoard[8]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[0]
+        } else if (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[4] == this.gameBoard[6]) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = this.gameBoard[2]
+        } else if(this.gameBoard.numPlays == 9) {
+            this.gameBoard.finished = true;
+            this.gameBoard.winPlayer = '0';
+        }   
+    },
+
+    endGame: function() {
+        if (this.gameBoard.winPlayer == '0') {
+            this.$displayMessage.textContent = "Draw!"
+        } else if (this.gameBoard.winPlayer == '1') {
+            this.$displayMessage.textContent = "Player " + this.gameBoard.winPlayer + " Wins!"
+            this.playerOneScore++;
+        } else {
+            this.$displayMessage.textContent = "Player " + this.gameBoard.winPlayer + " Wins!"
+            this.playerTwoScore++;
+        }
+    }
      //need bind events?
 
     
